490c5b07e1f6e80b3c15afbecfce195c1c9dd947
Pavlo Bernadin <pavlobernadin@gmail.com>
2026-06-07 21:41:05 +0300

    Фінальна назва файлів

---
+++ app.py
@@ -0,0 +1,14 @@
+from models import Expense
+import reports
+
+def main():
+    # Створюємо список витрат
+    my_expenses = [
+        Expense(150, "Їжа", "2026-06-07"),
+        Expense(500, "Транспорт", "2026-06-07")
+    ]
+    # Викликаємо функцію генерації звіту
+    reports.generate_report(my_expenses)
+
+if __name__ == "__main__":
+    main()ІІІ
