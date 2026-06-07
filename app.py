f3d9057d42148db5ed785b79b3b486ad5a91e07b
Pavlo.Design <pavlobernadin@gmail.com>
2026-06-07 20:42:41 +0300

    Update app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+from models import Expense
+import reports
+
+def main():
+    my_expenses = [
+        Expense(150, "Їжа", "2026-06-07"),
+        Expense(500, "Транспорт", "2026-06-07")
+    ]
+    reports.generate_report(my_expenses)
+
+if __name__ == "__main__":
+    main()
+  Додано код app.py
