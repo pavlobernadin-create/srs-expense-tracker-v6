294d82ce2a3bc2ff42e6744dd3e8b4dd9968ed22
Pavlo.Design <pavlobernadin@gmail.com>
2026-06-07 20:45:07 +0300

    Add Expense class to models.py

---
+++ models.py
@@ -0,0 +1,10 @@
+class Expense:
+    def __init__(self, amount, category, date):
+        self.amount = amount
+        self.category = category
+        self.date = date
+
+    def __str__(self):
+        return f"{self.date} | {self.category}: {self.amount} грн"
+      
+      Доданно models.py
