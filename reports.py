7095c4da555d52861f02921049a47aac012d27d2
Pavlo.Design <pavlobernadin@gmail.com>
2026-06-07 20:45:54 +0300

    reports.py

---
+++ reports.py
@@ -0,0 +1,8 @@
+class Expense:
+    def __init__(self, amount, category, date):
+        self.amount = amount
+        self.category = category
+        self.date = date
+
+    def __str__(self):
+        return f"{self.date} | {self.category}: {self.amount} грн"
