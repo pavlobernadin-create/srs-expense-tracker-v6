a60c69b8da1bf03fd9a6b003d49d6d5c366b35c7
Pavlo Bernadin <pavlobernadin@gmail.com>
2026-06-07 21:46:54 +0300

    Очищення: видалено файли з подвійним розширенням

--- models.py.py
+++
@@ -1,8 +0,0 @@
-class Expense:
-    def __init__(self, amount, category, date):
-        self.amount = amount
-        self.category = category
-        self.date = date
-
-    def __str__(self):
-        return f"{self.date} | {self.category}: {self.amount} грн"
