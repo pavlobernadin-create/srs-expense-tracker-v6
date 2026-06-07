c6eac72181a6662a8f47702e93056e8b011e3474
Pavlo.Design <pavlobernadin@gmail.com>
2026-06-07 21:01:52 +0300

    Delete main function and expense report logic
    Remove main function and related imports

--- app.py
+++ app.py
@@ -1,13 +1,1 @@
-from models import Expense
-import reports
 
-def main():
-    my_expenses = [
-        Expense(150, "Їжа", "2026-06-07"),
-        Expense(500, "Транспорт", "2026-06-07")
-    ]
-    reports.generate_report(my_expenses)
-
-if __name__ == "__main__":
-    main()
-  
